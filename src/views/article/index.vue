65194cbd6793d986e605d3d6742db660f1ddaa20
qiusiyuan0709 <2533805554@qq.com>
2019-06-30 00:26:18 +0800

    数据字段处理基本完成

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -11,14 +11,15 @@
                :model="form"
                label-width="80px">
         <el-form-item label="状态">
-          <el-radio-group v-model="form.resource">
-            <el-radio v-for="item in statTypes"
+          <el-radio-group v-model="filterParams.status">
+            <el-radio lable="">全部</el-radio>
+            <el-radio v-for="(item,index) in statTypes"
                       :key="item.label"
-                      :label="item.label"></el-radio>
+                      :label="index + ''">{{ item.label }}</el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-          <el-select v-model="form.region"
+          <el-select v-model="filterParams.channel_id"
                      placeholder="请选择活动区域">
             <el-option v-for="item in channels"
                        :key="item.id"
@@ -28,7 +29,9 @@
         </el-form-item>
 
         <el-form-item label="时间">
-          <el-date-picker v-model="form.value1"
+          <el-date-picker value-format="yyyy-MM-dd"
+                          v-model="begin_end_pubdate"
+                          @change="handleDateChange"
                           type="daterange"
                           range-separator="至"
                           start-placeholder="开始日期"
@@ -37,6 +40,7 @@
         </el-form-item>
         <el-form-item>
           <el-button type="primary"
+                     :loading="articleLoading"
                      @click="onSubmit">查询</el-button>
         </el-form-item>
       </el-form>
@@ -46,7 +50,7 @@
     <el-card class="list-card">
       <div slot="header"
            class="clearfix">
-        <span>共找到15条符合条件的内容</span>
+        <span>共找到<strong>{{ totalCount }}</strong>条符合条件的内容</span>
       </div>
 
       <!-- table 表格 -->
@@ -124,21 +128,10 @@ export default {
   data () {
     return {
       articles: [], // 列表数据
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-        value1: ''
-      },
-      totalCount: 0,
-      articleLoading: false,
-      page: 1,
-      statTypes: [
+      totalCount: 0, // 总数据条数
+      articleLoading: false, // 控制文档加载种的 loading 效果
+      page: 1, // 当前页码
+      statTypes: [ // 文章状态
         {
           type: 'info',
           label: '草稿'
@@ -160,7 +153,14 @@ export default {
           label: '已删除'
         }
       ],
-      channels: [] // 频道列表
+      channels: [], // 频道列表
+      filterParams: { // 文章查询条件参数
+        status: '', // 文章状态
+        channel_id: '', // 频道id
+        begin_pubdate: '', // 开始时间
+        end_pubdate: ''// 结束时间
+      },
+      begin_end_pubdate: [] // 存储日期选择器同步的 [开始时间, 结束时间], 这个字段没什么用，只是日期选择器必须v-model 绑定一个数据才会触发 change 事件
     }
   },
   created () {
@@ -173,12 +173,23 @@ export default {
   methods: {
     loadArticles (page = 1) { // 函数参数的默认值
       this.articleLoading = true
+
+      // 过滤出有效的查询条件数据字段
+      const filterData = {}
+      for (let key in this.filterParams) {
+        if (this.filterParams[key]) {
+          filterData[key] = this.filterParams[key]
+        }
+      }
+      console.log(filterData)
+
       this.$http({
         method: 'GET',
         url: '/articles',
         params: {
           page, // 请求数据的页码，不传默认为 1
-          per_page: 10 // 请求数据的每页大小，不传默认为10
+          per_page: 10, // 请求数据的每页大小，不传默认为10
+          ...filterData // 将对象混入当前对象，说白了就是对象拷贝
         }
       }).then(data => {
         this.articles = data.results // 列表数据
@@ -197,7 +208,7 @@ export default {
     },
 
     onSubmit () {
-      console.log('submit!')
+      this.loadArticles()
     },
 
     handleCurrentChange (page) {
@@ -230,6 +241,13 @@ export default {
           message: '已取消删除'
         })
       })
+    },
+    /**
+     * 日期选择组件改变事件
+     */
+    handleDateChange (value) {
+      this.filterParams.begin_pubdate = value[0]
+      this.filterParams.end_pubdate = value[1]
     }
   }
 }
